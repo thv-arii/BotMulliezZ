c41eab9eaf8e65060c5040573729863c57a86a42
thv-arii <119134561+thv-arii@users.noreply.github.com>
2022-11-27 15:24:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ printQRInTerminal: true,
 logger: pino({ level: 'silent' }),
 msgRetryCounterMap,
 auth: state,
-browser: ['MysticBot','Safari','9.7.0'],
+browser: ['BotMulliez','Safari','9.7.0'],
 getMessage: async (key) => (
 optss.store.loadMessage(/** @type {string} */(key.remoteJid), key.id) || 
 optss.store.loadMessage(/** @type {string} */(key.id)) || {}
